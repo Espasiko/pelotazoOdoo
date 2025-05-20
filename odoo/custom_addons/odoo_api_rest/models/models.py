cbef3ed26bde2fc1bae05319949c854943bc7c13
Espasiko <lorecomiendo11@gmail.com>
2025-05-20 15:27:43 +0200

    Delete odoo directory

--- odoo/custom_addons/odoo_api_rest/models/models.py
+++
@@ -1,5 +0,0 @@
-from odoo import models, fields, api
-
-# Este archivo puede contener extensiones de modelos existentes o nuevos modelos
-# Por ahora, no necesitamos crear modelos adicionales, ya que estamos utilizando
-# los modelos estándar de Odoo (product.template, res.partner, sale.order)
